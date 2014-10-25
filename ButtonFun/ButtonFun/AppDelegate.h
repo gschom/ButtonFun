f6aa24d3b11f4d16f8600a0c94e8f1dff5235700
Greg Schommer <grschom@gmail.com>
2014-10-25 14:11:14 -0400

    initial project

---
+++ ButtonFun/ButtonFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonFun
+//
+//  Created by Greg Schommer on 10/25/14.
+//  Copyright (c) 2014 Greg Schommer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonFun/ButtonFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonFun
+//
+//  Created by Greg Schommer on 10/25/14.
+//  Copyright (c) 2014 Greg Schommer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
