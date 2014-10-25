a96c50ddde7bbf8bfe0e290a7908bd9629086219
Greg Schommer <grschom@gmail.com>
2014-10-25 14:18:17 -0400

    Project cleanup. Hid status bar

--- ButtonFun/ButtonFun/MainViewController.h
+++ ButtonFun/ButtonFun/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  ButtonFun
 //
 //  Created by Greg Schommer on 10/25/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UIViewController
 
 @end
-
